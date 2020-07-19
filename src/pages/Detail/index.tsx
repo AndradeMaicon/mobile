a6061497e235288faf19dbb8f38de84fc5983138
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-07-19 12:09:45 -0300

    Page Details created

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { View, StyleSheet, Image, TouchableOpacity, Text, SafeAreaView } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
 
 const Detail = () => {
+  const navigation = useNavigation()
+
+  function handleNavigateBack() {
+    navigation.goBack()
+  }
+
   return (
-    <View />
+    <SafeAreaView style={{ flex: 1 }} >
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack} >
+          <Icon name="arrow-left" size={20} color="#34cd79" />
+        </TouchableOpacity>
+
+        <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=72'}} />
+
+        <Text style={styles.pointName}>Mercado do seu Ze</Text>
+        <Text style={styles.pointItems}>Lampadas, Oleo de Cozinha</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle} >Endereco</Text>
+          <Text style={styles.addressContent} > Rio do Sul, SC </Text>
+        </View>
+      </View>
+      <View style={styles.footer} >
+        <RectButton style={styles.button} onPress={() => {}} >
+          <FontAwesome name="whatsapp" size={20} color="#fff" />
+          <Text style={styles.buttonText} >Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}} >
+          <Icon name="mail" size={20} color="#fff" />
+          <Text style={styles.buttonText} >E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -11,7 +47,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20,
+    paddingTop: 40,
   },
 
   pointImage: {
